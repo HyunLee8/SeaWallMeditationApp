5709c753458ce2f2c9ab268c3f5e13b9e8e224cd
Isaac Lee <isaaclee@Isaacs-MacBook-Pro.local>
2025-10-11 19:41:06 -0400

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  base: "/Baseline",
 };
 
 export default nextConfig;
---
+++ src/app/menu/page.tsx
@@ -0,0 +1,19 @@
+import MagicBento from "../../components/MagicBento";
+export default function menu() {
+  return (
+    <div>
+      <MagicBento
+        textAutoHide={true}
+        enableStars={true}
+        enableSpotlight={true}
+        enableBorderGlow={true}
+        enableTilt={true}
+        enableMagnetism={true}
+        clickEffect={true}
+        spotlightRadius={300}
+        particleCount={12}
+        glowColor="132, 0, 255"
+      />
+    </div>
+  )
+}
